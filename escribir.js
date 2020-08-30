0397b44e61f0ada582a0a0bf2ad3164cc6665f98
Neson Ek <neltip.ne@gmail.com>
2020-08-30 18:40:08 -0500

    agregado comentarios al alchivo escribir.js

--- escribir.js
+++ escribir.js
@@ -6,4 +6,8 @@ const archivo = "text.txt"
 if (fs.existsSync(archivo)) {
     fs.writeFileSync(archivo,"Hola mundo");
     console.log("escrito correctemente");
+    console.log("nueno codigo necesario");
 }
+
+//este es un comentario nuevo
+//este es otro comentario
