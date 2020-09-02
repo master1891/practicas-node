175553e3b26401044584a2e14595276dc185c282
Nelson Ek <neltip.ne@gmail.com>
2020-09-01 22:55:23 -0500

    manejo de express, middleware y uso de nodemon

---
+++ express_ejemplos/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
+
+
+var app = express();
+
+//app.use(express.static("../ejemplos_web"));
+
+//configurando rutas del servidor
+
+
+const urlEncodedParser = bodyParser.urlencoded({extended:false});
+
+
+app.get('/',urlEncodedParser,(req,res) =>{
+    
+    res.send(`<h1>${req.query.nombre}</h1>`)
+    console.log(req.query);
+
+
+});
+
+app.get('/home',(req,res) =>{
+    res.send("Estas en la pagina de inicio");
+});
+
+
+app.listen(3000,() =>{
+    console.log('Servidor iniciado');
+});
+
